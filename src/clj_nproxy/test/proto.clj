4cf9460e33a5a551befc242698e2562ae83a0b6f
vhqr0 <zq_cmd@163.com>
2026-02-15 21:50:12 +0800

    proto test use pipe stream

--- src/clj_nproxy/test/proto.clj
+++ src/clj_nproxy/test/proto.clj
@@ -1,37 +1,18 @@
 (ns clj-nproxy.test.proto
   "Protocol test utils."
-  (:require [clojure.core.async :as a]
-            [clj-nproxy.struct :as st]
-            [clj-nproxy.proxy :as proxy])
+  (:require [clj-nproxy.proxy :as proxy])
   (:import [java.util.concurrent StructuredTaskScope StructuredTaskScope$Joiner]
-           [java.io BufferedInputStream BufferedOutputStream]))
-
-(defn ch->input-stream
-  "Convert channel to input stream."
-  [ch]
-  (BufferedInputStream.
-   (st/read-fn->input-stream #(a/<!! ch))))
-
-(defn ch->output-stream
-  "Convert channel to output stream."
-  [ch]
-  (BufferedOutputStream.
-   (st/write-fn->output-stream
-    (fn [b]
-      (let [b (bytes b)]
-        (when-not (zero? (alength b))
-          (a/>!! ch b))))
-    #(a/close! ch))))
+           [java.io PipedInputStream PipedOutputStream]))
 
 (defn handshake
-  "Run handshake on channel."
+  "Run handshake on internal pipe stream."
   [client-proc server-proc]
-  (let [c->s (a/chan 1024)
-        s->c (a/chan 1024)
-        cis (ch->input-stream s->c)
-        cos (ch->output-stream c->s)
-        sis (ch->input-stream c->s)
-        sos (ch->output-stream s->c)]
+  (with-open [cis (PipedInputStream.)
+              cos (PipedOutputStream.)
+              sis (PipedInputStream.)
+              sos (PipedOutputStream.)]
+    (.connect cos sis)
+    (.connect sos cis)
     (let [joiner (StructuredTaskScope$Joiner/allSuccessfulOrThrow)]
       (with-open [scope (StructuredTaskScope/open joiner)]
         (.fork scope ^Runnable #(client-proc {:input-stream cis :output-stream cos}))
@@ -39,7 +20,7 @@
         (.join scope)))))
 
 (defn proxy-handshake
-  "Run proxy handhsake on channel."
+  "Run proxy handhsake on internal pipe stream."
   ([client-opts server-opts]
    (proxy-handshake client-opts server-opts "example.com" 80))
   ([client-opts server-opts host port]
