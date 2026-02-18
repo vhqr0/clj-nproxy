a02c139af942d7f80361be684c50ab1f5de4947b
vhqr0 <zq_cmd@163.com>
2026-02-18 22:43:25 +0800

    improve vmess test

--- test/clj_nproxy/plugin/vmess_test.clj
+++ test/clj_nproxy/plugin/vmess_test.clj
@@ -8,22 +8,28 @@
 
 (set! clojure.core/*warn-on-reflection* true)
 
+(defn sim-vmess
+  [opts]
+  (let [uuid (str (random-uuid))]
+    (st/sim-conn
+     (fn [server]
+       (proxy/mk-client
+        (proxy/edn->client-opts (merge {:type :vmess :uuid uuid} opts))
+        server "example.com" 80
+        (fn [{^InputStream is :input-stream ^OutputStream os :output-stream}]
+          (.write os (bytes (b/rand 4)))
+          (.flush os)
+          (st/read-bytes is 4))))
+     (fn [client]
+       (proxy/mk-server
+        (proxy/edn->server-opts {:type :vmess :uuid uuid})
+        client
+        (fn [{^InputStream is :input-stream ^OutputStream os :output-stream}]
+          (let [b (bytes (st/read-bytes is 4))]
+            (.write os b)
+            (.flush os))))))))
+
 (deftest vmess-test
-  (is (some? (let [uuid (str (random-uuid))]
-               (st/sim-conn
-                (fn [server]
-                  (proxy/mk-client
-                   (proxy/edn->client-opts {:type :vmess :uuid uuid})
-                   server "example.com" 80
-                   (fn [{^InputStream is :input-stream ^OutputStream os :output-stream}]
-                     (.write os (bytes (b/rand 4)))
-                     (.flush os)
-                     (st/read-bytes is 4))))
-                (fn [client]
-                  (proxy/mk-server
-                   (proxy/edn->server-opts {:type :vmess :uuid uuid})
-                   client
-                   (fn [{^InputStream is :input-stream ^OutputStream os :output-stream}]
-                     (let [b (bytes (st/read-bytes is 4))]
-                       (.write os b)
-                       (.flush os))))))))))
+  (is (some? (sim-vmess {})))
+  (is (some? (sim-vmess {:sec :chacha20poly1305})))
+  (is (some? (sim-vmess {:use-mask? false :use-padding? false}))))
